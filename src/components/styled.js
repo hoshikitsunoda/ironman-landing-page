297413ab5eaa2e10af0e6fb1322b7a51fdfd42ff
Hosh'ki Tsunoda <liquidohr@gmail.com>
2019-09-21 16:12:07 -0700

    Create styled.js and add createGlobalStyle to normalize and import fonts #1

---
+++ src/components/styled.js
@@ -0,0 +1,30 @@
+import { createGlobalStyle } from 'styled-components'
+
+// Global
+
+export const GlobalStyle = createGlobalStyle`
+  @import url('https://fonts.googleapis.com/css?family=Nunito|Ropa+Sans&display=swap');
+
+  html {
+    background-color: #fffafa;
+  }
+  
+  html * {
+    font-family: 'Nunito', sans-serif;
+  }
+
+  h1,
+  h2,
+  h3,
+  h4,
+  p {
+    margin: 0 auto;
+  }
+
+  h1,
+  h2,
+  h3,
+  h4 {
+    font-family: 'Ropa Sans', sans-serif;
+  }
+`
