a4411cbfcbbd5779bcabe7df7d9109aad3fa6ae7
Hosh'ki Tsunoda <liquidohr@gmail.com>
2019-09-21 16:12:42 -0700

    Imported styled.js in App and added to app body #1

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from 'react'
 import './App.css'
 
+import * as Styled from './components/styled'
+
 function App() {
   return (
     <div className="App">
+      <Styled.GlobalStyle />
       <h1>Iron Man Landing Page</h1>
     </div>
   )
