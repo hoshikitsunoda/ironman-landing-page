048dba40d1af37826c7609367a678fffa180cf6f
Hosh'ki Tsunoda <liquidohr@gmail.com>
2019-09-21 16:17:47 -0700

    Created Hero component #1

---
+++ src/components/Hero.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+class Hero extends Component {
+  state = { isDesktop: false }
+  render() {
+    return (
+      <div>
+        <div>Hero Section</div>
+      </div>
+    )
+  }
+}
+
+export default Hero
