f9a1ffeffdd5c60d254a8b9e870465d97230b01d
Hosh'ki Tsunoda <liquidohr@gmail.com>
2019-09-22 03:41:51 -0700

    Map fetched data to populate ComicSlider #3

--- src/components/ComicSlider.js
+++ src/components/ComicSlider.js
@@ -1,11 +1,31 @@
 import React from 'react'
+import Slider from 'react-slick'
+
+import styled from 'styled-components'
 
 const ComicSlider = props => {
-  console.log(props.comicsData[0])
+  const settings = {
+    dots: false,
+    infinite: true,
+    autoplay: false,
+    slidesToShow: 6,
+    slidesToScroll: 3,
+    adaptiveHeight: true,
+    arrows: false,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      }
+    ]
+  }
+
   return (
-    <div>
+    <StyledSlider {...settings}>
       {props.comicsData.map((item, i) => {
-        console.log(item)
         const comicImagePath = item && item.images[0].path
         const comicImageExtension = item && item.images[0].extension
         const comicImage = `${comicImagePath}.${comicImageExtension}`
@@ -14,14 +34,32 @@ const ComicSlider = props => {
 
         return (
           <div key={i}>
-            <a href={link} target="_blank">
+            <a href={link} rel="noopener noreferrer" target="_blank">
               <img src={comicImage} alt={`comic-${i}`} />
             </a>
           </div>
         )
       })}
-    </div>
+    </StyledSlider>
   )
 }
+
+const StyledSlider = styled(Slider)`
+  .slick-slide {
+    margin: 0 1rem;
+  }
+
+  .slick-slide img {
+    width: 100%;
+  }
+
+  .slick-list {
+    padding: 2rem 20% 2rem 0;
+
+    @media (min-width: 767px) {
+      padding: 7rem 20% 4rem 0;
+    }
+  }
+`
 
 export default ComicSlider
