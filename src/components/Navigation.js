919156e45cd9c09bc049a7ba7a2c597392058bc8
Hosh'ki Tsunoda <liquidohr@gmail.com>
2019-09-21 18:42:50 -0700

    removed Logo svg image #1

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,11 +1,27 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import styled from 'styled-components'
+
+import Logo from './Logo'
 
 const Navigation = () => {
   return (
-    <div>
-      <div>Navigation</div>
-    </div>
+    <NavWrapper className="nav-wrapper">
+      <div className="logo">
+        <Logo />
+      </div>
+      <div className="linkWrapper">
+        <Link to="/">Sign In</Link>
+        <Link to="/">Sign Up</Link>
+      </div>
+    </NavWrapper>
   )
 }
+
+const NavWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`
 
 export default Navigation
