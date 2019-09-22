9a89465c57bca1cc34b0f104506e8ce63814a0a3
Hosh'ki Tsunoda <liquidohr@gmail.com>
2019-09-21 21:13:42 -0700

    Add heading text #2

--- src/components/Copy.js
+++ src/components/Copy.js
@@ -1,11 +1,25 @@
 import React from 'react'
 
+import styled from 'styled-components'
+
 const Copy = () => {
   return (
     <div>
-      <div>Copy</div>
+      <HeadingWrapper className="text-wrapper heading">
+        <p>Contrary to popular belief, he knows exactly what he’s doing.</p>
+      </HeadingWrapper>
     </div>
   )
 }
+
+const HeadingWrapper = styled.div`
+  text-align: center;
+  padding: 3rem;
+
+  p {
+    font-size: 1.5rem;
+    line-height: 2.2rem;
+  }
+`
 
 export default Copy
