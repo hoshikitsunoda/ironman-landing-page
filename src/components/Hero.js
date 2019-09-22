52020c1b1a278f9556bbaed36471f608aa65418c
Hosh'ki Tsunoda <liquidohr@gmail.com>
2019-09-21 17:43:20 -0700

    imported Navigation in Hero #1

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import Navigation from './Navigation'
 
 class Hero extends Component {
   state = { isDesktop: false }
@@ -7,6 +8,7 @@ class Hero extends Component {
     console.log(this.props.characterData)
     return (
       <div>
+        <Navigation />
         <div>Hero Section</div>
       </div>
     )
