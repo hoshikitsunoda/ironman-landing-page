6c119c90fc7e96c5041c503dfb203b1e59717d5f
Hosh'ki Tsunoda <liquidohr@gmail.com>
2019-09-21 17:32:37 -0700

    axios is now fetching IronMan data #1

--- src/App.js
+++ src/App.js
@@ -1,17 +1,46 @@
-import React from 'react'
+import React, { Component } from 'react'
 import './App.css'
+import axios from 'axios'
+import md5 from 'md5'
 
 import Hero from './components/Hero'
 
 import * as Styled from './components/styled'
 
-function App() {
-  return (
-    <div className="App">
-      <Styled.GlobalStyle />
-      <Hero />
-    </div>
-  )
+const characterCode = '1009368'
+const url = `https://gateway.marvel.com:443/v1/public/characters/${characterCode}`
+
+class App extends Component {
+  state = {}
+
+  async componentDidMount() {
+    let publicKey = process.env.REACT_APP_MARVEL_PUBLIC_API_KEY
+    let privateKey = process.env.REACT_APP_MARVEL_PRIVATE_API_KEY
+    let timestamp = process.env.REACT_APP_MARVEL_TIMESTAMP
+    let hash = md5(timestamp + privateKey + publicKey)
+    try {
+      const res = await axios.get(url, {
+        params: {
+          ts: timestamp,
+          apikey: publicKey,
+          hash: hash
+        }
+      })
+      const { data } = await res
+      console.log(data)
+    } catch (err) {
+      console.error(err)
+    }
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Styled.GlobalStyle />
+        <Hero />
+      </div>
+    )
+  }
 }
 
 export default App
