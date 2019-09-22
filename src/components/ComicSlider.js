a9bf70f56917a26e11eb2a9a962ba49acd381bde
Hosh'ki Tsunoda <liquidohr@gmail.com>
2019-09-22 02:13:48 -0700

    populated comic images with link to original page #3

--- src/components/ComicSlider.js
+++ src/components/ComicSlider.js
@@ -1,14 +1,27 @@
-import React, { Component } from 'react'
+import React from 'react'
 
-class ComicSlider extends Component {
-  state = { comicData: [] }
-  render() {
-    return (
-      <div>
-        <div>ComicSlider</div>
-      </div>
-    )
-  }
+const ComicSlider = props => {
+  console.log(props.comicsData[0])
+  return (
+    <div>
+      {props.comicsData.map((item, i) => {
+        console.log(item)
+        const comicImagePath = item && item.images[0].path
+        const comicImageExtension = item && item.images[0].extension
+        const comicImage = `${comicImagePath}.${comicImageExtension}`
+
+        const link = item.urls[0].url
+
+        return (
+          <div key={i}>
+            <a href={link} target="_blank">
+              <img src={comicImage} alt={`comic-${i}`} />
+            </a>
+          </div>
+        )
+      })}
+    </div>
+  )
 }
 
 export default ComicSlider
