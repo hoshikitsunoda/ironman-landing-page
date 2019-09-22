f3277b24f197b1d2344eef10ef36f3e614e82392
Hosh'ki Tsunoda <liquidohr@gmail.com>
2019-09-22 01:47:55 -0700

    changed axios to fetch two url at once #3

--- src/App.js
+++ src/App.js
@@ -10,29 +10,40 @@ import ComicSlider from './components/ComicSlider'
 import * as Styled from './components/styled'
 
 const characterCode = '1009368'
-const url = `https://gateway.marvel.com:443/v1/public/characters/${characterCode}`
+const urlCharacter = `https://gateway.marvel.com:443/v1/public/characters/${characterCode}`
+const urlComics = `https://gateway.marvel.com:443/v1/public/comics?titleStartsWith=Iron%20Man`
 
 class App extends Component {
-  state = { characterData: [] }
+  state = { characterData: [], comicsData: [] }
 
   async componentDidMount() {
     let publicKey = process.env.REACT_APP_MARVEL_PUBLIC_API_KEY
     let privateKey = process.env.REACT_APP_MARVEL_PRIVATE_API_KEY
     let timestamp = process.env.REACT_APP_MARVEL_TIMESTAMP
     let hash = md5(timestamp + privateKey + publicKey)
+
     try {
-      const res = await axios.get(url, {
-        params: {
-          ts: timestamp,
-          apikey: publicKey,
-          hash: hash
-        }
-      })
-      const { data } = await res
+      const [characterRes, comicsRes] = await Promise.all([
+        axios.get(urlCharacter, {
+          params: {
+            ts: timestamp,
+            apikey: publicKey,
+            hash: hash
+          }
+        }),
+        axios.get(urlComics, {
+          params: {
+            ts: timestamp,
+            apikey: publicKey,
+            hash: hash
+          }
+        })
+      ])
+
       this.setState({
-        characterData: data.data.results[0]
+        characterData: characterRes.data.data.results[0],
+        comicsData: comicsRes.data.data.results
       })
-      console.log(this.state.characterData)
     } catch (err) {
       console.error(err)
     }
