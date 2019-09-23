ba994690710472e9e8e5095d870e6aeb882ee304
Hosh'ki Tsunoda <liquidohr@gmail.com>
2019-09-22 23:10:30 -0700

    slick slide styling updated #7

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,11 +1,8 @@
 import React from 'react'
 import { BrowserRouter as Router, Link } from 'react-router-dom'
 import styled from 'styled-components'
-import * as Styled from './styled'
 
 import Logo from './Logo'
-
-console.log(Styled.Device.mobile)
 
 const Navigation = () => {
   return (
