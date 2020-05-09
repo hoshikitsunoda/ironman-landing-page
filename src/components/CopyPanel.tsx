df8901b0a1165207d33b5ee0b17fe9af82f66f68
Hosh'ki Tsunoda <hoshkitsunoda@gmail.com>
2020-05-09 20:07:11 +0900

    made Panel class and created new instances for each panel #13

--- src/components/CopyPanel.tsx
+++ src/components/CopyPanel.tsx
@@ -17,31 +17,35 @@ interface PanelItem {
   [key: string]: PanelData
 }
 
+class Panel {
+  constructor(
+    public icon: string,
+    public heading: string,
+    public body: string
+  ) {}
+}
+
 const panelData: PanelItem = {
-  genius: {
-    icon: GeniusIcon,
-    heading: 'Super-Genius Intelligence',
-    body:
-      'Quite apart from the powers granted him by the suit, Tony Stark is far more than a mechanical engineering prodigy who graduated from the Massachusetts Institute of Technology with honors at the age of 17.',
-  },
-  tactician: {
-    icon: TacticianIcon,
-    heading: 'Expert Tactician',
-    body:
-      'He is a brilliant tactician capable of quickly formulating battle strategies and new plans if the situation changes, like being able to elaborate complex plans in order to defeat different enemies.',
-  },
-  engineer: {
-    icon: EngineerIcon,
-    heading: 'Master Engineer',
-    body:
-      'He is an excellent engineer and mechanic capable of fixing almost any, if not all machinery.',
-  },
-  businessman: {
-    icon: BusinessmanIcon,
-    heading: 'Master Businessman',
-    body:
-      "Stark is extremely well-respected in the business world, able to command people's attentions when he speaks on economic matters.  He has built up several multi-million dollar companies from virtually nothing.",
-  },
+  genius: new Panel(
+    GeniusIcon,
+    'Super-Genius Intelligence',
+    'Quite apart from the powers granted him by the suit, Tony Stark is far more than a mechanical engineering prodigy who graduated from the Massachusetts Institute of Technology with honors at the age of 17.'
+  ),
+  tactician: new Panel(
+    TacticianIcon,
+    'Expert Tactician',
+    'He is a brilliant tactician capable of quickly formulating battle strategies and new plans if the situation changes, like being able to elaborate complex plans in order to defeat different enemies.'
+  ),
+  engineer: new Panel(
+    EngineerIcon,
+    'Master Engineer',
+    'He is an excellent engineer and mechanic capable of fixing almost any, if not all machinery.'
+  ),
+  businessman: new Panel(
+    BusinessmanIcon,
+    'Master Businessman',
+    "Stark is extremely well-respected in the business world, able to command people's attentions when he speaks on economic matters.  He has built up several multi-million dollar companies from virtually nothing."
+  ),
 }
 
 const CopyPanel: React.FC = () => {
