ea016eac27c3601275d14ff028ecc6636ce345a2
Hosh'ki Tsunoda <hoshkitsunoda@gmail.com>
2020-05-10 00:32:51 +0900

    added type to all const in api.tsx #15

--- src/data/api.tsx
+++ src/data/api.tsx
@@ -1,7 +1,9 @@
-const characterCode = '1009368'
-export const urlCharacter = `https://gateway.marvel.com:443/v1/public/characters/${characterCode}`
-export const urlComics = `https://gateway.marvel.com:443/v1/public/comics?titleStartsWith=Iron%20Man`
+const characterCode: string = '1009368'
+export const urlCharacter: string = `https://gateway.marvel.com:443/v1/public/characters/${characterCode}`
+export const urlComics: string = `https://gateway.marvel.com:443/v1/public/comics?titleStartsWith=Iron%20Man`
 
-export const publicKey = process.env.REACT_APP_MARVEL_PUBLIC_API_KEY
-export const privateKey = process.env.REACT_APP_MARVEL_PRIVATE_API_KEY
-export const timestamp = process.env.REACT_APP_MARVEL_TIMESTAMP!
+export const publicKey: string | undefined =
+  process.env.REACT_APP_MARVEL_PUBLIC_API_KEY
+export const privateKey: string | undefined =
+  process.env.REACT_APP_MARVEL_PRIVATE_API_KEY
+export const timestamp: string = process.env.REACT_APP_MARVEL_TIMESTAMP!
