9f2d4489c379f996070dbe160866147afffeb504
Hosh'ki Tsunoda <hoshkitsunoda@gmail.com>
2020-05-11 23:59:17 +0900

    added more types to fetchData function #15

--- src/utils/api.tsx
+++ src/utils/api.tsx
@@ -1,11 +1,19 @@
-import axios from 'axios'
+import axios, { AxiosResponse } from 'axios'
 import { urlCharacter, urlComics, paramsSetting } from '../data/api'
+import { CharacterProps, ComicsProps } from '../models/Props'
 
-const fetchData = async () => {
-  let charaResult = axios.get(urlCharacter, paramsSetting)
-  let comicsResult = axios.get(urlComics, paramsSetting)
+const fetchData = async (): Promise<AxiosResponse<{}>[]> => {
+  let charaResult: Promise<AxiosResponse<CharacterProps>> = axios.get<
+    CharacterProps
+  >(urlCharacter, paramsSetting)
+  let comicsResult: Promise<AxiosResponse<ComicsProps[]>> = axios.get<
+    ComicsProps[]
+  >(urlComics, paramsSetting)
 
-  const promises = [Promise.resolve(charaResult), Promise.resolve(comicsResult)]
+  const promises: Promise<AxiosResponse<{}>>[] = [
+    Promise.resolve(charaResult),
+    Promise.resolve(comicsResult),
+  ]
 
   return await axios.all(promises)
 }
