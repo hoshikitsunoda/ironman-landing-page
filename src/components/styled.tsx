f18a47280bbb1f90bbea352304226cc6e5cc88d8
Hosh'ki Tsunoda <hoshkitsunoda@gmail.com>
2020-05-12 00:53:35 +0900

    added mobile size #17

--- src/components/styled.tsx
+++ src/components/styled.tsx
@@ -40,12 +40,14 @@ export const GlobalStyle = createGlobalStyle`
 `
 
 const size = {
+  mobile: '350px',
   tablet: '768px',
   laptop: '1024px',
   laptopL: '1200px',
 }
 
 export const Device = {
+  mobile: `(min-width: ${size.mobile})`,
   tablet: `(min-width: ${size.tablet})`,
   laptop: `(min-width: ${size.laptop})`,
   laptopL: `(min-width: ${size.laptopL})`,
