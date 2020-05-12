c68399edbdf9445eb05cdbc78a4d491ce0d94e76
Hosh'ki Tsunoda <hoshkitsunoda@gmail.com>
2020-05-13 02:09:48 +0900

    added tests to Navigation #19

--- src/test/Navigation.test.tsx
+++ src/test/Navigation.test.tsx
@@ -1,23 +1,39 @@
 import React from 'react'
-import { render, fireEvent, waitForElement } from '@testing-library/react'
+import { render } from '@testing-library/react'
+import { createMemoryHistory } from 'history'
+import { Router } from 'react-router-dom'
 import { ThemeProvider } from 'styled-components'
+
 import * as Styled from '../components/styled'
 
 import Navigation from '../components/Navigation'
 
 const renderNavigation = () => {
+  const history = createMemoryHistory()
   return (
     <ThemeProvider theme={Styled.theme}>
-      <Navigation />
+      <Router history={history}>
+        <Navigation />
+      </Router>
     </ThemeProvider>
   )
 }
 
 describe('<Navigation />', () => {
-  test('renders Navigation component', async () => {
-    const { getAllByText } = render(renderNavigation())
+  test('renders SVG', async () => {
+    const { queryByTestId } = render(renderNavigation())
 
-    const items = await getAllByText(/Sign/)
-    expect(items).toHaveLength(2)
+    expect(queryByTestId('svg')).toBeTruthy()
+  })
+})
+
+describe('<Navigation />', () => {
+  test('checks both links have correct href', async () => {
+    const { getByText } = render(renderNavigation())
+
+    const signIn = getByText('Sign In').closest('a') as HTMLAnchorElement
+    const signUp = getByText('Sign Up').closest('a') as HTMLAnchorElement
+    expect(signIn.href).toMatch('/signin')
+    expect(signUp.href).toMatch('/signup')
   })
 })
