f4b25049ad08c84a3af1d5bf7209026033d5f836
Hosh'ki Tsunoda <hoshkitsunoda@gmail.com>
2020-05-13 23:23:37 +0900

    added tests for CopyPanel component #19

---
+++ src/__test__/CopyPanel.test.tsx
@@ -0,0 +1,50 @@
+import React from 'react'
+import { mount } from 'enzyme'
+import CopyPanel from '../components/CopyPanel'
+import { panelData } from '../data/seed'
+
+import { ThemeProvider } from 'styled-components'
+import * as Styled from '../components/styled'
+
+describe('<CopyPanel />', () => {
+  const wrapper = mount(
+    <ThemeProvider theme={Styled.theme}>
+      <CopyPanel />
+    </ThemeProvider>
+  )
+
+  it('should render component', () => {
+    expect(wrapper).toMatchSnapshot()
+  })
+
+  it('should have 4 children', () => {
+    expect(wrapper.find('.panel').hostNodes()).toHaveLength(4)
+  })
+
+  it('should have correspinding image source', () => {
+    const imgSrc = wrapper.find('img').map((item) => item.prop('src'))
+    expect(imgSrc).toEqual(
+      Object.keys(panelData).map(
+        (key: string, i: number) => panelData[key].icon
+      )
+    )
+  })
+
+  it('should have correspinding headers', () => {
+    const texts = wrapper.find('h4').map((node) => node.text())
+    expect(texts).toEqual(
+      Object.keys(panelData).map(
+        (key: string, i: number) => panelData[key].heading
+      )
+    )
+  })
+
+  it('should have correspinding headers', () => {
+    const texts = wrapper.find('p').map((node) => node.text())
+    expect(texts).toEqual(
+      Object.keys(panelData).map(
+        (key: string, i: number) => panelData[key].body
+      )
+    )
+  })
+})
