a5ad947c243bd5c21e9b90b142d38a62cd71a9a4
Hosh'ki Tsunoda <hoshkitsunoda@gmail.com>
2020-05-14 01:40:52 +0900

    added beforeEach to Hero test #19

---
+++ src/__test__/Hero.test.tsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import { mount, ReactWrapper } from 'enzyme'
+import Hero from '../components/Hero'
+import Navigation from '../components/Navigation'
+
+import { ThemeProvider } from 'styled-components'
+import * as Styled from '../components/styled'
+import ironManCartoon from '../images/Iron-man-Cartoon.png'
+
+describe('<Hero />', () => {
+  const heroProps = {
+    characterData: { description: 'Lorem ipsum dolor sit amet.' },
+  }
+  let wrapper: ReactWrapper
+  beforeEach(() => {
+    wrapper = mount(
+      <ThemeProvider theme={Styled.theme}>
+        <Hero {...heroProps} />
+      </ThemeProvider>
+    )
+  })
+
+  it('should render component', () => {
+    expect(wrapper).toMatchSnapshot()
+  })
+
+  it('should render Navigation component', () => {
+    expect(wrapper.find(Navigation)).toHaveLength(1)
+  })
+
+  it('should render Ironman image', () => {
+    expect(wrapper.find('img').props().src).toEqual(ironManCartoon)
+  })
+})
