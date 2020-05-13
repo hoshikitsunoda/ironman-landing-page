334efa4a66eafef08d28da905c7dc4b9518ab55c
Hosh'ki Tsunoda <hoshkitsunoda@gmail.com>
2020-05-14 01:27:16 +0900

    added beforeEach to Copy test #19

---
+++ src/__test__/Copy.test.tsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import { mount, ReactWrapper } from 'enzyme'
+import Copy from '../components/Copy'
+import CopyPanel from '../components/CopyPanel'
+
+import { ThemeProvider } from 'styled-components'
+import * as Styled from '../components/styled'
+
+describe('<Copy />', () => {
+  const copyProps = {
+    characterData: { thumbnail: { path: 'ironman', extension: 'jpg' } },
+  }
+  let wrapper: ReactWrapper
+  beforeEach(() => {
+    wrapper = mount(
+      <ThemeProvider theme={Styled.theme}>
+        <Copy {...copyProps} />
+      </ThemeProvider>
+    )
+  })
+
+  it('should render component', () => {
+    expect(wrapper).toMatchSnapshot()
+  })
+
+  it('should render heading paragraph', () => {
+    expect(wrapper.find('.heading').hostNodes()).toHaveLength(1)
+  })
+
+  it('should render CopyPanel component', () => {
+    expect(wrapper.find('.thumbnail')).toHaveLength(1)
+  })
+
+  it('should render CopyPanel component', () => {
+    expect(wrapper.find(CopyPanel)).toHaveLength(1)
+  })
+})
