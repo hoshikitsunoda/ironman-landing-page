5a86a9bd00d7a2ac931711bfc4287bbecaf79b18
Hosh'ki Tsunoda <hoshkitsunoda@gmail.com>
2020-05-13 15:10:28 +0900

    updated jest config #19

--- jest.config.js
+++ jest.config.js
@@ -1,19 +1,10 @@
 module.exports = {
-  // The root of your source code, typically /src
-  // `<rootDir>` is a token Jest substitutes
   roots: ['<rootDir>/src'],
-
-  // Jest transformations -- this adds support for TypeScript
-  // using ts-jest
   transform: {
     '^.+\\.tsx?$': 'ts-jest',
   },
-
-  // Test spec file resolution pattern
-  // Matches parent folder `__tests__` and filename
-  // should contain `test` or `spec`.
-  testRegex: '(/tests/.*|(\\.|/)(test|spec))\\.tsx?$',
-
-  // Module file extensions for importing
+  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  snapshotSerializers: ['enzyme-to-json/serializer'],
+  setupFiles: ['<rootDir>/tools/jest/setup-react-adapter.js'],
 }
