0d054027b481bdcf091955373e052f08a2747115
Hosh'ki Tsunoda <hoshkitsunoda@gmail.com>
2020-05-14 16:16:59 +0900

    added matchMedia config to setupTests so slider can be tested #19

--- src/setupTests.tsx
+++ src/setupTests.tsx
@@ -2,3 +2,13 @@ import { configure } from 'enzyme'
 import * as ReactSixteenAdapter from 'enzyme-adapter-react-16'
 const adapter = ReactSixteenAdapter as any
 configure({ adapter: new adapter.default() })
+
+window.matchMedia =
+  window.matchMedia ||
+  function () {
+    return {
+      matches: false,
+      addListener: function () {},
+      removeListener: function () {},
+    }
+  }
