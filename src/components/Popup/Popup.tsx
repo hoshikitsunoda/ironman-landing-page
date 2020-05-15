f7275a67f57faddb5ce4eb78feb97d993e818fef
Hosh'ki Tsunoda <hoshkitsunoda@gmail.com>
2020-05-16 01:39:26 +0900

    created basic popup component #21

---
+++ src/components/Popup/Popup.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+import styled from 'styled-components'
+import * as Styled from '../../components/styled'
+
+const Popup = () => {
+  return <div>Work in progress</div>
+}
+
+export default Popup
