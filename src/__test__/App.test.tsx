24d243a2c3b3cf3d23c09c3fcc376b22c4e1f167
Hosh'ki Tsunoda <hoshkitsunoda@gmail.com>
2020-05-15 22:16:42 +0900

    added tests for App component #19

---
+++ src/__test__/App.test.tsx
@@ -0,0 +1,52 @@
+import React from 'react'
+import { mount, ReactWrapper } from 'enzyme'
+
+import App from '../App'
+import Hero from '../components/Hero'
+import Copy from '../components/Copy'
+import ComicSlider from '../components/ComicSlider'
+
+import { characterObject, comicsObject } from '../data/variables'
+
+import { ThemeProvider } from 'styled-components'
+import * as Styled from '../components/styled'
+
+jest.mock('axios')
+
+const characterDataStructure = {
+  characterData: characterObject,
+}
+
+const comicsDataStructure = {
+  comicsData: comicsObject,
+}
+
+describe('<App />', () => {
+  let wrapper: ReactWrapper
+  beforeEach(() => {
+    wrapper = mount(
+      <ThemeProvider theme={Styled.theme}>
+        <App />
+      </ThemeProvider>
+    )
+  })
+
+  it('should render component', () => {
+    expect(wrapper).toMatchSnapshot()
+  })
+
+  it('should render Hero component with correct props', () => {
+    expect(wrapper.find(Hero)).toHaveLength(1)
+    expect(wrapper.find(Hero).props()).toEqual(characterDataStructure)
+  })
+
+  it('should render Copy component with correct props', () => {
+    expect(wrapper.find(Copy)).toHaveLength(1)
+    expect(wrapper.find(Copy).props()).toEqual(characterDataStructure)
+  })
+
+  it('should render ComicSlider component with correct props', () => {
+    expect(wrapper.find(ComicSlider)).toHaveLength(1)
+    expect(wrapper.find(ComicSlider).props()).toEqual(comicsDataStructure)
+  })
+})
